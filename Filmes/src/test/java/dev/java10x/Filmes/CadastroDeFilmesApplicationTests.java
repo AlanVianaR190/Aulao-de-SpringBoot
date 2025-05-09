49dfd81789982c83898eca90f6e519a13e14ad84
AlanVianaR190 <152330025+AlanVianaR190@users.noreply.github.com>
2025-05-09 15:42:07 -0300

    add

---
+++ Filmes/src/test/java/dev/java10x/Filmes/CadastroDeFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
